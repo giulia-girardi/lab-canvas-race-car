771826a18bc51948fec303d592df83b31c9387ab
Giulia Girardi <gg.giuliagirardi@gmail.com>
2022-10-13 14:00:34 +0200

    finish iteration1

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
+const canvas = document.querySelector('canvas')
+const ctx = canvas.getContext('2d')
+
+const background = new Image();
+background.src = '../images/road.png'
+
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
+    console.log('it works')
     startGame();
   };
 
-  function startGame() {}
+  function startGame() {
+    ctx.drawImage(background, 0, 0, 500, 700)
+  }
 };
