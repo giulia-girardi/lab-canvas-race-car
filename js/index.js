56c269a6db1b1b9af40c21ac01059ee6a28fc5c9
Giulia Girardi <gg.giuliagirardi@gmail.com>
2022-10-13 14:32:36 +0200

    iteration2

--- js/index.js
+++ js/index.js
@@ -3,14 +3,16 @@ const ctx = canvas.getContext('2d')
 
 const background = new Image();
 background.src = '../images/road.png'
+const car = new Image();
+car.src = '../images/car.png'
 
 window.onload = () => {
   document.getElementById('start-button').onclick = () => {
-    console.log('it works')
     startGame();
   };
 
   function startGame() {
     ctx.drawImage(background, 0, 0, 500, 700)
+    ctx.drawImage(car, canvas.width / 2 - 35, 430, 70, 150)
   }
 };
